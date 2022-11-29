6a167aaba2ec06948193ff94d0d4e1e555e87ed1
Nikita-front-end <117522710+Nikita-front-end@users.noreply.github.com>
2022-11-29 19:03:04 +0300

    change #1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-(function() {
+(function () {
     const header = document.querySelector('.header');
     window.onscroll = () => {
-        if (window.pageYOffset  > 85) {
+        if (window.pageYOffset > 85) {
             header.classList.add('header_active');
         } else {
             header.classList.remove('header_active');
@@ -11,10 +11,10 @@
 
 // Burger handler
 
-(function (){
+(function () {
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header_nav');
-    const menuCloseItem = document.querySelector ('.header_nav-close');
+    const menuCloseItem = document.querySelector('.header_nav-close');
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header_nav_active');
     });
@@ -27,24 +27,24 @@
 (function () {
 
     const smoothScroll = function (targetEl, duration) {
-        const headerElHeight =  document.querySelector('.header').clientHeight;
+        const headerElHeight = document.querySelector('.header').clientHeight;
         let target = document.querySelector(targetEl);
         let targetPosition = target.getBoundingClientRect().top - headerElHeight;
         let startPosition = window.pageYOffset;
         let startTime = null;
-    
-        const ease = function(t,b,c,d) {
+
+        const ease = function (t, b, c, d) {
             t /= d / 2;
             if (t < 1) return c / 2 * t * t + b;
             t--;
             return -c / 2 * (t * (t - 2) - 1) + b;
         };
-    
-        const animation = function(currentTime){
+
+        const animation = function (currentTime) {
             if (startTime === null) startTime = currentTime;
             const timeElapsed = currentTime - startTime;
             const run = ease(timeElapsed, startPosition, targetPosition, duration);
-            window.scrollTo(0,run);
+            window.scrollTo(0, run);
             if (timeElapsed < duration) requestAnimationFrame(animation);
         };
         requestAnimationFrame(animation);
